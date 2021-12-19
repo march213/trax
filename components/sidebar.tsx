890d318ce9f3f9a37795eb9cda29a5ac35c0a843
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-12-18 22:54:22 -0500

    menus

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,14 +1,48 @@
 import NextImage from 'next/image'
+import NextLink from 'next/link'
 import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from '@chakra-ui/layout'
 import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md'
 
+const navMenu = [
+  {
+    name: 'Home',
+    icon: MdHome,
+    route: '/',
+  },
+  {
+    name: 'Search',
+    icon: MdSearch,
+    route: '/search',
+  },
+  {
+    name: 'Your Library',
+    icon: MdLibraryMusic,
+    route: '/library',
+  },
+]
+
 const Sidebar = () => {
   return (
-    <Box width="100%" height="calc(100vh - 100px)" bg="blackAlpha.900" px="2" color="gray.300">
-      <Box py="5">
-        <Box width="120px" mb="5" px="5">
+    <Box width="100%" height="calc(100vh - 100px)" px={2} color="gray.300" bg="blackAlpha.900">
+      <Box py={5}>
+        <Box width="120px" mb={5} px={5}>
           <NextImage src="/trax-logo.svg" height={60} width={120} />
-          Trax
+        </Box>
+        <Box mb={5}>
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem key={menu.name} px={5} fontSize="16px">
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon as={menu.icon} mr={5} color="white" />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
