2508f557909f27728193ab93cfa09b40337fa907
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-12-20 23:46:39 -0500

    add generic fetcher

---
+++ lib/fetcher.ts
@@ -0,0 +1,10 @@
+export function fetcher(url: string, data = undefined) {
+  return fetch(`${window.location.origin}/api${url}`, {
+    method: data ? 'POST' : 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+}
