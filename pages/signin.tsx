644fa59feb0504542ebfa3fddbada67bf406a640
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-12-21 00:04:23 -0500

    setting up auth

---
+++ pages/signin.tsx
@@ -0,0 +1,8 @@
+import { AuthForm } from '../components/authForm'
+
+const SigninPage = () => {
+  return <AuthForm mode="signin" />
+}
+
+SigninPage.authPage = true
+export default SigninPage
---
+++ pages/signup.tsx
@@ -0,0 +1,8 @@
+import { AuthForm } from '../components/authForm'
+
+const SignupPage = () => {
+  return <AuthForm mode="signup" />
+}
+
+SignupPage.authPage = true
+export default SignupPage
