8a0003038d92bdd2a28ab9b11767252c38306668
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-12-21 00:46:17 -0500

    protect route

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken'
+import { NextApiRequest, NextApiResponse } from 'next'
+import PrismaClient from './prisma'
+
+export function validateRoute(handler) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const { trax_access_token: token } = req.cookies
+    if (token) {
+      let user
+
+      try {
+        const { id } = jwt.verify(token, process.env.JWT_SECRET)
+        user = await PrismaClient.user.findUnique({
+          where: { id },
+        })
+        if (!user) {
+          throw new Error('User not found')
+        }
+      } catch (e) {
+        res.status(401)
+        res.json({ error: 'Not Authorized' })
+        return
+      }
+
+      return handler(req, res, user)
+    }
+
+    res.status(401)
+    res.json({ error: 'Not Authorized' })
+  }
+}
