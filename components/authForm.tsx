477a54aed7ca3ff322db0e5ea6a9dbd2165c672c
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-12-21 00:32:24 -0500

    use uncontrolled inputs

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,21 +1,30 @@
 import { FC, useState } from 'react'
 import NextImage from 'next/image'
-import { Box, Flex, Input, Button } from '@chakra-ui/react'
+import { Box, Flex, Input, Button, Text } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { useSWRConfig } from 'swr'
 import { auth } from '../lib/mutations'
 
 const AuthForm: FC<{ mode: 'signin' | 'signup' }> = ({ mode }) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(undefined)
   const router = useRouter()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     setIsLoading(true)
 
+    const email = e.target.elements?.email?.value
+    const password = e.target.elements?.password?.value
+
+    if (!email || !password) {
+      setError('Please enter an email and password')
+      setIsLoading(false)
+      return
+    }
+
     await auth(mode, { email, password })
+    setError(undefined)
     setIsLoading(false)
     router.push('/')
   }
@@ -28,25 +37,17 @@ const AuthForm: FC<{ mode: 'signin' | 'signup' }> = ({ mode }) => {
       <Flex justify="center" align="center" height="calc(100vh - 100px)">
         <Box padding="12" bg="gray.900" borderRadius="6px">
           <form onSubmit={handleSubmit}>
-            <Input
-              mb="4"
-              borderColor="gray.600"
-              type="email"
-              placeholder="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              mb="4"
-              borderColor="gray.600"
-              type="password"
-              placeholder="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <Input mb="4" borderColor="gray.600" name="email" type="email" placeholder="email" />
+            <Input mb="4" borderColor="gray.600" name="password" type="password" placeholder="password" />
+            {error ? (
+              <Text mb="4" color="red.400">
+                {error}
+              </Text>
+            ) : null}
             <Button
               type="submit"
               bg="green.500"
               isLoading={isLoading}
-              disabled={!email || !password}
               _hover={{ backgroundColor: 'green.400' }}
               _focus={{ backgroundColor: 'green.600' }}
               _active={{ backgroundColor: 'green.600' }}
