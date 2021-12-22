751bd2e12a54d6a7050819faebd21325f2f5bf69
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-12-22 01:13:14 -0500

    gradient layout

---
+++ components/gradientLayout.tsx
@@ -0,0 +1,30 @@
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
+
+const GradientLayout = ({ title, subtitle, color, image, children, description, roundImage }) => {
+  return (
+    <Box
+      height="100%"
+      overflowY="auto"
+      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0, 0, 0, 0.9) 75%)`}
+    >
+      <Flex bg={`${color}.600`} padding="10" align="end">
+        <Box padding="5">
+          <Image src={image} boxSize="160px" boxShadow="2xl" borderRadius={roundImage ? '50%' : '4px'} />
+        </Box>
+        <Box padding="5" lineHeight="40px" color="white">
+          <Text fontSize="xs" fontWeight="bold" casing="uppercase">
+            {subtitle}
+          </Text>
+          <Text fontSize="6xl" fontWeight="bold">
+            {title}
+          </Text>
+          <Text fontSize="xs">{description}</Text>
+        </Box>
+      </Flex>
+      {children}
+    </Box>
+  )
+}
+
+export { GradientLayout }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from 'next/head'
+import { GradientLayout } from '../components/gradientLayout'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Trax</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>home</main>
-    </div>
+    <GradientLayout
+      color="gray"
+      subtitle="profile"
+      title="Jane M"
+      description="15 public playlists"
+      image="https://res.cloudinary.com/dh5nhyzyb/image/upload/v1640153483/Screen_Shot_2021-12-22_at_1.10.35_AM.png"
+      roundImage
+    >
+      <div>home page</div>
+    </GradientLayout>
   )
 }
